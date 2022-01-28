a7f2bc619fcd4d3d0a6dd482e72d818e32a43880
Joey Deckers <deckersjoey@gmail.com>
2022-01-28 20:26:11 +0100

    Initial Commit

---
+++ Shared/Instagram_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Joey Deckers on 28/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
